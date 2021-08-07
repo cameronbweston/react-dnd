9e9bd49c1b45599c918546e2c8704a69650543e6
Cameron Weston <cameronbweston@gmail.com>
2021-08-07 16:12:21 -0500

    Finish DnD react part 2

--- src/pages/ClassDetails/ClassDetails.jsx
+++ src/pages/ClassDetails/ClassDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { getDetails } from "../../services/api-calls";
 
 class ClassDetails extends Component {
   state = {
@@ -9,13 +10,33 @@ class ClassDetails extends Component {
 
   async componentDidMount() {
     // This is where our API call will live
+    const classDetails = await getDetails(this.state.url)
     // Then we'll set state!
+    this.setState({ classDetails })
   }
 
-  render() {
+  render() { 
+    //Look at this nifty functional programming happiness!!
+    const { classDetails } = this.state;
+    
     return (
       <div>
-        <h3>Class Details</h3>
+        { classDetails.name ?
+        <>
+        {/* Gotta have the random cat pics... */}
+        <img src="http://theoldreader.com/kittens/320/240/" alt=""/>
+        <h2>{classDetails.name}</h2>
+        <div>Hit die: d{classDetails.hit_die}</div>
+        <h3>Proficiencies:</h3>
+        {classDetails.proficiencies.map((proficiency) => (
+          <div key={proficiency.index}>{proficiency.name}</div>
+        ))}
+        </>
+        :
+        <>
+        <p>Loading class details...</p>
+        </>
+        }
       </div>
     );
   }
